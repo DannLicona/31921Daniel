132fe6ac001cf2baafcc0479ae3ca9f843955ffa
DannLicona <daniellicona34@aragon.unam.mx>
2023-11-30 10:39:48 -0600

    Se agrega index

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="Estilo.css">
+    <title>CRUD</title>
+</head>
+<body>
+
+    <h1>Bienvenido a mi pagina para registros</h1>
+
+    <nav>
+        <ul>
+            <li><a href="modulos/usuarios/index.php">Iniciar Sesión</a></li>
+            <li><a href="modulos/usuarios/registro.php">Registrarse</a></li>
+            <li><a href="modulos/usuarios/datos.php">Ver Usuarios</a></li>
+        </ul>
+    </nav>
+
+</body>
+</html>
